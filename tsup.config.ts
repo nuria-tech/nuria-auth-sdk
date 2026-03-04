bf8bea3e8983b2962581ab71238c2c0a591e9ece
Arthur Saito <arthur.saito@nuria.com.br>
2026-03-03 21:01:48 -0300

    build target to ES2018
    Configures TypeScript and tsup to transpile code to ES2018. This ensures broader compatibility for the SDK output.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  target: 'es2018',
   dts: true,
   clean: true,
   sourcemap: true,
